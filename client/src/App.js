4f8240aabec1331a34f17e5ebc923aa941144577
annoy004 <avsingh02as02@gmail.com>
2025-10-02 14:30:29 +0530

    Add Google OAuth environment variable support for production deployment

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import AccountProvider from './context/accountprovider.jsx';
 
 function App() {
 
-  const clientId = '453626519073-ca6d59jjoskudtm62v380uvho4l2qqla.apps.googleusercontent.com';
+  const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID || '453626519073-ca6d59jjoskudtm62v380uvho4l2qqla.apps.googleusercontent.com';
   return (
     <GoogleOAuthProvider clientId={clientId}>
       <AccountProvider>
