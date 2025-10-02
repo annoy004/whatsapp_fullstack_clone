94d0ac2599b6d230cd69746cc04e31194f2c129c
annoy004 <avsingh02as02@gmail.com>
2025-10-02 15:54:29 +0530

    Trigger redeploy for updated OAuth settings

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import AccountProvider from './context/accountprovider.jsx';
 
 
 function App() {
-
+  // Updated for production deployment
   const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID || '453626519073-ca6d59jjoskudtm62v380uvho4l2qqla.apps.googleusercontent.com';
   return (
     <GoogleOAuthProvider clientId={clientId}>
